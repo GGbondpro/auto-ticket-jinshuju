60829c2d1d6d1f9ddbd352ae91a8a4e38cfd28ca
Jasperwang525 <2961043594@qq.com>
2023-09-22 18:29:24 +0800

    金数据快速报名

--- auto_fill.py
+++ auto_fill.py
@@ -1,56 +1,54 @@
-page_num = 4 # 你需要评价的同学的页数
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-print("imported normally")
 
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver,200)
 # 如果网址有变, 请自行修改
-driver.get("http://172.31.126.2/login?redirect=/user/profile")
+driver.get("https://jinshuju.net/f/GPDuXZ")
+# 定位元素
+name = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/form/div[3]/div/div[2]/div/div/div[2]/div[1]/div/div/div/span/input')))
+phone=wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/form/div[3]/div/div[4]/div/div/div[2]/div[1]/div/div/div/div/span/input')))
+student_id=wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/form/div[3]/div/div[6]/div/div/div[2]/div[1]/div/div/div/span/input')))
+id=wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/form/div[3]/div/div[8]/div/div/div[2]/div[1]/div/div[2]/div/span/input')))
+campus=wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/form/div[3]/div/div[10]/div/div/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/label')))
+button=wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/form/div[5]/div/button')))
 
-username=driver.find_element(by="xpath",value="/html/body/div/div/form/div[1]/div/div/input")
-password = driver.find_element(by="xpath",value="/html/body/div/div/form/div[2]/div/div[1]/input")
+#清空文本框
+name.clear()
+phone.clear()
+student_id.clear()
+id.clear()
+#填写文本框
+name.send_keys("王鑫培")
+phone.send_keys("17841733149")
+student_id.send_keys("3022244362")
+id.send_keys("440983200310308018")
+#一些按钮
+campus.click()
+button.click()
+#等待提交成功
+time.sleep(50)
+print("successfully!")
 
-# 下面两行输入你的用户名和密码，两者皆输入你的学号，后续不需要输验证码
-username.clear()
-password.clear()
-username.send_keys("")
-password.send_keys("")
-# 你有20秒完成: 点击 ，登录, 点击左侧"班级互评"按钮, 如果不够, 请修改sleep后面的数字(秒)
-print("waiting for logging in")
-time.sleep(20)
-driver.implicitly_wait(2)
+driver.quit()
 
-grades = ["25", "45", "10", "10", "10"]
-# 一轮只能处理一个页面的同学
-def fill_in_one_page(page):
-    for i in range(0, 10):
-        nextpage_button = driver.find_element(by=By.CLASS_NAME, value="btn-next")
-        for p in range(1, page):
-            nextpage_button.click()
 
-        time.sleep(1)
-        print(f"dealing with the {i}th student")
-        links = driver.find_elements(by=By.CLASS_NAME, value="action")
-        links[i].click()
-        
-        blanks = driver.find_elements(by=By.CLASS_NAME, value="el-input__inner")
-        count = 0
-        for blank in blanks:
-            if count < 5:
-                blank.clear()
-                blank.send_keys(grades[count])
-                count += 1
-            else:
-                break
-        button = driver.find_element(by=By.CLASS_NAME, value="el-button--primary")
-
-        button.click()
-        driver.back()
-
-for pn in range(1, page_num+1):
-    fill_in_one_page(pn)
-
-print("finish")
+"""
+/html/body/div[1]/div/div/form/div[3]/div/div[2]/div/div/div[2]/div[1]/div/div/div/span/input
+/html/body/div[1]/div/div/form/div[3]/div/div[4]/div/div/div[2]/div[1]/div/div/div/div/span/input
+/html/body/div[1]/div/div/form/div[3]/div/div[6]/div/div/div[2]/div[1]/div/div/div/span/input
+/html/body/div[1]/div/div/form/div[3]/div/div[8]/div/div/div[2]/div[1]/div/div[2]/div/span/input
+/html/body/div[1]/div/div/form/div[3]/div/div[10]/div/div/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/label
+/html/body/div[1]/div/div/form/div[5]/div/button
+"""
+"""
+/html/body/div[1]/div/div/form/div[3]/div/div[2]/div/div/div[2]/div[1]/div/div/div/span/input
+/html/body/div[1]/div/div/form/div[3]/div/div[4]/div/div/div[2]/div[1]/div/div/div/div/span/input
+/html/body/div[1]/div/div/form/div[3]/div/div[6]/div/div/div[2]/div[1]/div/div/div/span/input
+/html/body/div[1]/div/div/form/div[3]/div/div[8]/div/div/div[2]/div[1]/div/div[2]/div/span/input
+/html/body/div[1]/div/div/form/div[3]/div/div[10]/div/div/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/label
+/html/body/div[1]/div/div/form/div[5]/div/button
+"""
