51a474f9d541f461835fa26f56129efce8692d12
Jasperwang525 <2961043594@qq.com>
2023-09-22 20:29:49 +0800

    优化提速

--- auto_fill.py
+++ auto_fill.py
@@ -2,12 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime, timedelta
 import time
 
 driver = webdriver.Chrome()
-wait = WebDriverWait(driver,200)
+driver.maximize_window()
+wait = WebDriverWait(driver,2000)
 # 如果网址有变, 请自行修改
-driver.get("https://jinshuju.net/f/GPDuXZ")
+driver.get("https://jinshuju.net/f/LgvX7x")
+
+
+# 定义目标时间
+target_datetime = datetime(2023, 9, 22, 20, 29, 00)
+
+# 获取当前时间
+current_datetime = datetime.now()
+
+# 计算时间差
+time_difference = target_datetime - current_datetime
+
+# 将时间差转换为秒
+target_time = time.time() + time_difference.total_seconds()
+
+# 循环等待，直到目标时间到达
+while time.time() < target_time:
+    continue
+
+driver.refresh()
+
 # 定位元素
 name = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/form/div[3]/div/div[2]/div/div/div[2]/div[1]/div/div/div/span/input')))
 phone=wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/form/div[3]/div/div[4]/div/div/div[2]/div[1]/div/div/div/div/span/input')))
@@ -22,15 +44,15 @@ phone.clear()
 student_id.clear()
 id.clear()
 #填写文本框
-name.send_keys("")
-phone.send_keys("")
-student_id.send_keys("")
-id.send_keys("")
+name.send_keys("哇哦")
+phone.send_keys("18889292938")
+student_id.send_keys("3243323422")
+id.send_keys("39208445")
 #一些按钮
 campus.click()
 button.click()
 #等待提交成功
-time.sleep(50)
+time.sleep(5)
 print("successfully!")
 
 driver.quit()
